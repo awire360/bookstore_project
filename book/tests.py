1b01489a4edd2d7c06c7c3b6d3cecefa9aec01e1
awire360 <andy.warner@outlook.com>
2025-02-05 17:58:05 +0000

    Register Book model in admin and add initial tests for model methods

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from book.models import Book
 
-# Create your tests here.
+class BookModelTests(TestCase):
+    def test_create_book(self):
+        book = Book.objects.create(
+            title="Test Title",
+            author="Test Author",
+            description="Test Description",
+            inventory=5
+        )
+        self.assertEqual(book.title, "Test Title")
+
+    def test_str_method(self):
+        book = Book.objects.create(
+            title="String Method Test",
+            author="Test Author",
+            description="Test",
+            inventory=1
+        )
+        self.assertEqual(str(book), "String Method Test")
